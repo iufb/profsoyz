59cf43b50babcc90315b46a1751c6128c822068c
iufb <bashirov3ld2@gmail.com>
2024-11-08 11:58:48 +0500

    fix: card

--- src/widgets/Cards/Card.tsx
+++ src/widgets/Cards/Card.tsx
@@ -30,7 +30,7 @@ export const Card = ({
       className={cn(
         {
           horizontal:
-            "w-full grid grid-cols-[1fr_2fr] gap-2 h-[200px]  after:bottom-0 after:top-0   after:left-0 after:-right-[6px]  ",
+            "w-full grid grid-cols-1 grid-rows-[1fr,auto]  md:grid-cols-[1fr_2fr] gap-2 h-[200px]  after:bottom-0 after:top-0   after:left-0 after:-right-[6px]  ",
           base: "flex w-full  h-[308px] flex-col after:-bottom-[6px] after:top-0 after:left-0 after:right-0  ",
         }[variant],
         "after:bg-base4 after:-z-10 after:rounded-md after:absolute text-base2  bg-base6 p-4 rounded-md relative min-h-[300px] ",
@@ -39,7 +39,7 @@ export const Card = ({
       <div
         className={cn(
           {
-            base: "flex-grow-1 h-[70%] ",
+            base: "flex-grow-1 h-[80%] ",
             horizontal: "col-start-1 col-end-2 ",
           }[variant],
           "relative ",
@@ -56,12 +56,14 @@ export const Card = ({
       </div>
       <div>
         <span className="text-sm">{date}</span>
-        <h2 className="font-bold text-md">
-          {title.length > 50 ? `${title.slice(0, 50)}...` : title}
+        <h2 className="font-bold text-center text-md">
+          {title.length > 70 ? `${title.slice(0, 70)}...` : title}
         </h2>
-        <p className="text-md">
-          {content.length > 90 ? `${content.slice(0, 90)}...` : content}
-        </p>
+        {variant == "horizontal" && (
+          <p className="text-md hidden md:block">
+            {content.length > 410 ? `${content.slice(0, 410)}...` : content}
+          </p>
+        )}
       </div>
     </Comp>
   );
