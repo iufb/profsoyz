72a4ad1f79f438a3851dbf246f517620f13f3224
iufb <bashirov3ld2@gmail.com>
2024-12-13 11:01:48 +0500

    fix styles:horizontal card

--- src/widgets/Cards/Card.tsx
+++ src/widgets/Cards/Card.tsx
@@ -31,10 +31,10 @@ export const Card = ({
       className={cn(
         {
           horizontal:
-            "w-full grid grid-cols-1 grid-rows-[1fr,auto]  md:grid-cols-[1fr_2fr] gap-2 h-[200px]  after:bottom-0 after:top-0   after:left-0 after:-right-[6px]  ",
-          base: "flex w-full  h-[308px] flex-col after:-bottom-[6px] after:top-0 after:left-0 after:right-0  ",
+            " h-full md:h-[250px] grid grid-cols-1  grid-rows-[1fr,auto]  md:grid-cols-[auto_1fr] gap-2   after:bottom-0 after:top-0   after:left-0 after:-right-[6px] w-[90%]  ",
+          base: "flex w-full  h-[308px] flex-col after:-bottom-[6px] after:top-0 after:left-0 after:right-0   ",
         }[variant],
-        "after:bg-base4 after:-z-10 after:rounded-md after:absolute text-base2  bg-base6 p-2 rounded-md relative min-h-[400px] ",
+        "after:bg-base4 after:-z-10 after:rounded-md after:absolute text-base2  bg-base6 p-2 rounded-md relative   ",
       )}
     >
       <div
@@ -43,41 +43,53 @@ export const Card = ({
             base: "flex-grow-1 h-[80%] ",
             horizontal: "col-start-1 col-end-2 ",
           }[variant],
-          "relative ",
+          "relative  ",
         )}
       >
-        <Image
-          src={`${backendImageUrl}/${image}`}
-          fill
-          style={{ objectFit: "cover" }}
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          alt={title}
-          className="absolute rounded-sm left-0 right-0 top-0 bottom-0 "
-        />
+        {variant == "base" ? (
+          <Image
+            src={`${backendImageUrl}/${image}`}
+            fill
+            style={{ objectFit: "cover" }}
+            alt={title}
+            className="absolute rounded-sm left-0 right-0 top-0 bottom-0 "
+          />
+        ) : (
+          <Image
+            src={`${backendImageUrl}/${image}`}
+            width={230}
+            height={230}
+            className="rounded-sm"
+            alt={title}
+          />
+        )}
       </div>
       <div>
         <span className="text-sm">{date}</span>
         <h2
           className={clsx(
             "font-bold text-center text-md",
-            variant == "horizontal" && "text-lg md:text-2xl",
+            variant == "horizontal" && "text-lg md:text-xl",
           )}
         >
           {title.length > 70 ? `${title.slice(0, 70)}...` : title}
         </h2>
         {variant == "horizontal" && (
-          <div
-            className={clsx(
-              "quill-content",
-              "overflow-x-auto overflow-y-hidden p-10",
-            )}
-            dangerouslySetInnerHTML={{
-              __html: content
-                .replace(/&lt;/g, "<")
-                .replace(/&gt;/g, ">")
-                .replace(/&amp;nbsp;/g, " "),
-            }}
-          />
+          <>
+            <div
+              className={clsx(
+                "quill-content",
+                "overflow-x-auto overflow-y-hidden p-10",
+              )}
+              dangerouslySetInnerHTML={{
+                __html: content
+                  .replace(/&lt;/g, "<")
+                  .replace(/&gt;/g, ">")
+                  .replace(/&amp;nbsp;/g, " "),
+              }}
+            />
+            <div className="w-full h-[2px] bg-slate-200" />
+          </>
         )}
       </div>
     </Comp>
