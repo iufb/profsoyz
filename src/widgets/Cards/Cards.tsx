46c481d554fe9df7dfec10979a5034cb00fe044c
iufb <bashirov3ld2@gmail.com>
2025-03-04 10:30:17 +0500

    cards view  reverse

--- src/widgets/Cards/Cards.tsx
+++ src/widgets/Cards/Cards.tsx
@@ -1,65 +1,65 @@
 "use client";
 import { cn } from "@/shared/lib/utils";
-import { Card, CardProps } from "./Card";
-import { useState } from "react";
-import { useTranslations } from "next-intl";
 import clsx from "clsx";
+import { useTranslations } from "next-intl";
 import { useParams } from "next/navigation";
+import { useState } from "react";
+import { Card, CardProps } from "./Card";
 
 export interface CardsProps {
-  title: string;
-  variant: "base" | "horizontal";
-  items: Omit<CardProps, "variant">[];
+    title: string;
+    variant: "base" | "horizontal";
+    items: Omit<CardProps, "variant">[];
 }
 function CardsClient({ title, variant, items }: CardsProps) {
-  const [max, setMax] = useState(3);
-  const t = useTranslations();
-  const params = useParams();
+    const [max, setMax] = useState(3);
+    const t = useTranslations();
+    const params = useParams();
 
-  const forNews =
-    title.includes("жаңалықтар") ||
-    title.includes("новости") ||
-    params.slug.includes("home");
+    const forNews =
+        title.includes("жаңалықтар") ||
+        title.includes("новости") ||
+        params.slug.includes("home");
 
-  return (
-    <>
-      <section
-        className={clsx("flex scrollbar flex-col gap-3 ", forNews && "")}
-      >
-        <h2 className="text-2xl text-base2 font-bold">{title}</h2>
-        <div
-          className={cn(
-            variant == "base"
-              ? "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3   gap-10 "
-              : "flex flex-col gap-2",
-          )}
-        >
-          {items.slice(0, max).map((i, idx) => (
-            <Card key={idx} variant={variant} {...i} />
-          ))}
-        </div>
-      </section>
-      {forNews && (
-        <div className="flex justify-center text-base2 gap-10">
-          {max > 3 && <button onClick={() => setMax(3)}>{t("less")}</button>}
-          {items.length > items.slice(0, max).length && (
-            <button
-              onClick={() =>
-                setMax((prev) => {
-                  if (items.length <= items.slice(0, max).length) {
-                    return prev;
-                  }
-
-                  return prev + 3;
-                })
-              }
+    return (
+        <>
+            <section
+                className={clsx("flex scrollbar flex-col gap-3 ", forNews && "")}
             >
-              {t("more")}
-            </button>
-          )}
-        </div>
-      )}
-    </>
-  );
+                <h2 className="text-2xl text-base2 font-bold">{title}</h2>
+                <div
+                    className={cn(
+                        variant == "base"
+                            ? "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3   gap-10 "
+                            : "flex flex-col gap-2",
+                    )}
+                >
+                    {items.reverse().slice(0, max).map((i, idx) => (
+                        <Card key={idx} variant={variant} {...i} />
+                    ))}
+                </div>
+            </section>
+            {forNews && (
+                <div className="flex justify-center text-base2 gap-10">
+                    {max > 3 && <button onClick={() => setMax(3)}>{t("less")}</button>}
+                    {items.length > items.slice(0, max).length && (
+                        <button
+                            onClick={() =>
+                                setMax((prev) => {
+                                    if (items.length <= items.slice(0, max).length) {
+                                        return prev;
+                                    }
+
+                                    return prev + 3;
+                                })
+                            }
+                        >
+                            {t("more")}
+                        </button>
+                    )}
+                </div>
+            )}
+        </>
+    );
 }
 export default CardsClient;
